c2a4a03157d5c06cc8a04c4f5df66a227c0f9d38
wangjun <91237498+wangjun-1998@users.noreply.github.com>
2022-08-02 06:43:01 +0000

    first update on 20220802

--- {{cookiecutter.app_name}}/classifier.py
+++ {{cookiecutter.app_name}}/classifier.py
@@ -288,5 +288,5 @@ if __name__ == '__main__':
     # Display the results
     print("Plotting the results")
     plot(results)
-    print('今天是2022.07.04')
-    print('wuxi hold on!')
+    print('今天是2022.08.02')
+    print('wuxi hhh')
