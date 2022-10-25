aedf06c9d9e49332fe54895bc2626f09585cb75f
wangjun <91237498+wangjun-1998@users.noreply.github.com>
2022-10-25 07:22:30 +0000

    first updating on 2022/10/25

--- {{cookiecutter.app_name}}/classifier.py
+++ {{cookiecutter.app_name}}/classifier.py
@@ -288,5 +288,5 @@ if __name__ == '__main__':
     # Display the results
     print("Plotting the results")
     plot(results)
-    print('今天是2022.10.20')
-    print('Audi-A7')
+    print('今天是2022.10.25')
+    print('Audi-S7')
