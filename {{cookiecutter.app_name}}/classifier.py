55d265e15e3fe51f4d3631aa9a72119c4fd8a08a
wangjun <91237498+wangjun-1998@users.noreply.github.com>
2023-06-20 06:23:44 +0000

    first updating on 2023/06/20

--- {{cookiecutter.app_name}}/classifier.py
+++ {{cookiecutter.app_name}}/classifier.py
@@ -288,5 +288,5 @@ if __name__ == '__main__':
     # Display the results
     print("Plotting the results")
     plot(results)
-    print('今天是2023.06.16')
-    print('MABAH-S450')
+    print('今天是2023.06.20')
+    print('porsche-911')
