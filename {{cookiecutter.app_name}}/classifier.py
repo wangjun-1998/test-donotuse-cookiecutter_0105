d6bceb8b758248cd5730c61b49e3ba54a9d81e95
wangjun <91237498+wangjun-1998@users.noreply.github.com>
2023-09-01 06:52:15 +0000

    Update classifier.py on 2023/09/01

--- {{cookiecutter.app_name}}/classifier.py
+++ {{cookiecutter.app_name}}/classifier.py
@@ -288,5 +288,5 @@ if __name__ == '__main__':
     # Display the results
     print("Plotting the results")
     plot(results)
-    print('今天是2023.08.10')
-    print('benz-cls')
+    print('今天是2023.09.01')
+    print('Accord')
