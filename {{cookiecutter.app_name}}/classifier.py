2fdd1d93176d25892fd4ceff9fe7d1cca386f3b4
wangjun <91237498+wangjun-1998@users.noreply.github.com>
2023-09-21 07:33:18 +0000

    Update classifier.py on 20203/09/21

--- {{cookiecutter.app_name}}/classifier.py
+++ {{cookiecutter.app_name}}/classifier.py
@@ -288,5 +288,5 @@ if __name__ == '__main__':
     # Display the results
     print("Plotting the results")
     plot(results)
-    print('今天是2023.09.19')
-    print('Audi-s7')
+    print('今天是2023.09.21')
+    print('vivo-s90')
